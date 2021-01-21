a42a129ebc51a853e4414b99b2103de886f9efda
mgstabrani <mgstabrani19@gmail.com>
2021-01-21 22:59:13 +0700

    Add function isSingleLetter

---
+++ src/function.py
@@ -0,0 +1,11 @@
+from input import *
+
+def isSingleLetter(x):
+    found = 0
+    for letter in firstOperand,secondOperand,result:
+        if(x == letter):
+            found += 1
+    if(found > 1):
+        return False
+    else:
+        return True
