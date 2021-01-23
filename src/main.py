e5f7f0f52bd84a5d830d6434c64eb25293691a1c
mgstabrani <mgstabrani19@gmail.com>
2021-01-23 11:59:15 +0700

    Add time library

--- src/main.py
+++ src/main.py
@@ -1,5 +1,9 @@
 from function import *
-
+import timeit
+start = timeit.default_timer()
 putLetters()
 initNumber()
 heapPermutation(list(range(10)),10,10)
+stop = timeit.default_timer()
+timeExecution = stop - start
+print("Time Execution =", timeExecution, "second")
